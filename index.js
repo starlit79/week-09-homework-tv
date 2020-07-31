3c79659923dd111d35436249f227ee9db4e84aaa
Sharon Lin <hsiang.lin@gmail.com>
2020-07-31 15:05:28 -0400

    Peaked and compared with level 2, and was able to get the season page to run

--- index.js
+++ index.js
@@ -11,11 +11,16 @@ app.set('view engine', 'pug')
 app.get('/', (request, response) => {
   return response.render('index', { showdata })
 })
+//in level 2 {showdata} was not written in line 12
 
-app.get('/seasons/:number', (request, response) => {
-    const seasonNum = showdata.seasons.find((seasonNum) => seasonNum.number === parseInt(request.params.number))
+app.get('/season/:number', (request, response) => {
+    const season = showdata.seasons.find((season) => season.number === parseInt(request.params.number))
     
-    return response.render('season', { seasonNum })
+    return response.render('season', { season, title: showdata.title })
+    //looked at level 2 for this part, and changing variable name from seasonNum to season to avoid confusion
+    //destructuring title: showdata.title? need some explanation how this info is communicating with season.pug
+    //'season' is pulling season.pub file?
+    //what is render doing in this case?
 })
 
 app.all('*', (request, response) => {
